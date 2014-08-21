db85c6897b7c120d3be8ca9c1665ed12863ec189
GregorStocks <gregor@gregorstocks.com>
2014-08-20 23:34:30 -0700

    Fix indentation. This improves engagement.

--- valley-scroll.js
+++ valley-scroll.js
@@ -1,6 +1,6 @@
 window.onscroll=function() {
   var now = new Date().getTime();
-    while (new Date().getTime() < now + 300) {
-      // allow the user's eyes to rest on the content, which improves engagement
-    }
+  while (new Date().getTime() < now + 300) {
+    // allow the user's eyes to rest on the content, which improves engagement
+  }
 };
