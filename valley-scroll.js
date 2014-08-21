4a6a61e4ae709f9b08e28d2c431037e65634593d
GregorStocks <gregor@gregorstocks.com>
2014-08-20 23:29:14 -0700

    valley-scroll.js

--- valley-scroll.js
+++ valley-scroll.js
@@ -1,4 +1,6 @@
-var now = new Date().getTime();
-while (new Date().getTime() < now + 300) {
-  // allow the user's eyes to rest on the content, which improves engagement
-}
+window.onscroll=function() {
+  var now = new Date().getTime();
+    while (new Date().getTime() < now + 300) {
+      // allow the user's eyes to rest on the content, which improves engagement
+    }
+};
